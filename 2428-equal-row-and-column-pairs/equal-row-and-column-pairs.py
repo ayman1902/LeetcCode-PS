77a06554659010fa34e941b6d5b2f1086e64c2cb
ayman1902 <49163010+ayman1902@users.noreply.github.com>
2024-08-05 14:00:16 +0100

    Time: 382 ms (85.37%) | Memory: 16.1 MB (36.81%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.py
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.py
@@ -13,7 +13,7 @@ class Solution(object):
             
         for j in range(len(grid[0])):
             col_hashmap[hash(tuple([grid[i][j] for i in range(len(grid))]))]+=1
-        for key,value in row_hashmap.items():
+        for key,value in row_hashmap.items():#it appear in both way so dreb binathoum xd
             if key in col_hashmap:
                 res+=value*col_hashmap[key]
         return res
