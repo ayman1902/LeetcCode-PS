ea1dabebb7ec7e2834e41949a0990177a216f91f
ayman1902 <49163010+ayman1902@users.noreply.github.com>
2024-08-07 14:32:23 +0100

    Time: 120 ms (16.34%) | Memory: 15.7 MB (20.51%) - LeetSync

---
+++ 338-counting-bits/counting-bits.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def countBits(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        def bit(number):
+            res=0
+            while number>0:
+                res+=number%2
+                number=number//2
+            return res
+
+        res=[0]*(n+1)
+        for i in range(n+1):
+            res[i]=bit(i)
+        return res
