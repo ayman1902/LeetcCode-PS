bdefb68e1cdbaeca7aba9dac459972d5af28b8f8
ayman1902 <49163010+ayman1902@users.noreply.github.com>
2024-08-14 10:47:14 +0100

    Time: 72 ms (59.15%) | Memory: 15.8 MB (57.93%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -10,7 +10,8 @@ class Solution(object):
             anares=''.join(sorted(strs[i]))
             if anares not in hashmap:
                 hashmap[anares]=[]
-            hashmap[anares].append(i)
-        for key,value in hashmap.items():
-            res.append([strs[val] for val in value])
-        return res
+            hashmap[anares].append(strs[i])
+        return hashmap.values()
+        #for key,value in hashmap.items():
+        #    res.append([strs[val] for val in value])
+        #eturn res
