b8a4b8522cce81aa046016f1a2a09f77042c7903
ayman1902 <49163010+ayman1902@users.noreply.github.com>
2024-08-19 11:30:02 +0100

    Time: 39 ms (75.54%) | Memory: 12.9 MB (5.32%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -8,15 +8,10 @@ class Solution(object):
         magazin_hashmap=defaultdict(int)
         for i in range(len(magazine)):
             magazin_hashmap[magazine[i]]+=1
-        print(magazin_hashmap)
         for i in range(len(ransomNote)):
             if ransomNote[i] in magazin_hashmap and magazin_hashmap[ransomNote[i]]>0:
                 magazin_hashmap[ransomNote[i]]-=1
             else:
                 return False
         return True
-        print(magazin_hashmap)
-        
-
-        print(magazin_hashmap)
         
