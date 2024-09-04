f35830cc4d9ccfb6d4147fce00bf60c34e568ef6
ayman1902 <49163010+ayman1902@users.noreply.github.com>
2024-09-04 19:05:33 +0100

    Time: 13 ms (81.17%) | Memory: 11.8 MB (62.70%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -5,22 +5,17 @@ class Solution(object):
         :rtype: str
         """
         strs.sort()
-        print(strs)
         res=0
         hashmap=defaultdict(int)
-        print(hashmap)
         min_lenght=min([len(strings) for strings in strs])
-        print(min_lenght)
         for i in range(min_lenght):
             for strings in strs:
                 hashmap[strings[i]]+=1
-            print(hashmap,len(hashmap))
             if len(hashmap) ==1:
                 res+=1
             else:
                 break
             hashmap.clear()
-        print(res)
         if res==0:
             return ""
         return strs[0][:res]
