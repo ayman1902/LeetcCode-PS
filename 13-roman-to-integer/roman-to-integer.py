6266f7bf401f6413d6c16b30a1b82e4afd0f52af
ayman1902 <49163010+ayman1902@users.noreply.github.com>
2024-09-05 18:48:39 +0100

    Time: 47 ms (5.11%) | Memory: 11.6 MB (84.56%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        res=0
+        hashmap={
+            "I":1,
+            "V":5,
+            "X":10,
+            "L":50,
+            "C":100,
+            "D":500,
+            "M":1000
+        }
+        print(hashmap)
+        for i in range(len(s)-1,-1,-1):
+            current_value=hashmap[s[i]]
+            if i+1<len(s) and current_value<hashmap[s[i+1]]:
+                res-=current_value
+            else:
+                res+=current_value
+            print(current_value)
+            
+        return res
