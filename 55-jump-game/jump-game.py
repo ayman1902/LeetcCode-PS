80dd0bbf90654fcda7b7cf5daf99cf78e335b0e7
ayman1902 <49163010+ayman1902@users.noreply.github.com>
2024-09-11 19:21:21 +0100

    Time: 369 ms (27.90%) | Memory: 12.8 MB (47.08%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -6,9 +6,9 @@ class Solution(object):
         """
         furthest=0
         for i in range(len(nums)):
-            if i > furthest:
+            if i > furthest:#ila kan aslan rak mat9derch twssel l index so false
                 return False
-            furthest=max(furthest,i+nums[i])
+            furthest=max(furthest,i+nums[i])#update il kan matupdati assat
         return True
         
             
