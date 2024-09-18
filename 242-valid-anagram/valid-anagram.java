02d0ddc24b4adee1b9c004644c89916a30f0bcec
ayman1902 <49163010+ayman1902@users.noreply.github.com>
2024-09-18 19:13:34 +0100

    Time: 27 ms (5.39%) | Memory: 44.3 MB (54.37%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -6,14 +6,10 @@ class Solution {
         }
         HashMap<Character, Integer> hashmapS = new HashMap<Character, Integer>();
         HashMap<Character, Integer> hashmapT = new HashMap<Character, Integer>();
-        System.out.println(hashmapS);
-        System.out.println(hashmapT);
         for (int i = 0; i < s.length(); i++) {
             hashmapS.put(s.charAt(i), hashmapS.getOrDefault(s.charAt(i), 0) + 1);
             hashmapT.put(t.charAt(i), hashmapT.getOrDefault(t.charAt(i), 0) + 1);
         }
-        System.out.println(hashmapS);
-        System.out.println(hashmapT);
         return hashmapS.equals(hashmapT);
     }
 }
