31eb9a11c307c2ecefd326847a70a5e1e679ce22
ayman1902 <49163010+ayman1902@users.noreply.github.com>
2024-09-21 19:28:36 +0100

    Time: 418 ms (6.62%) | Memory: 12.1 MB (7.26%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        n=len(s)
+        if n==1:
+            return s
+        index=1
+        track_matrix=[[s[i],0] for i in range(n)]
+        going_down=False
+        hashmap=defaultdict(str)
+        for i in range(len(track_matrix)):
+            track_matrix[i][1]=index
+            hashmap[index]+=s[i]
+            if going_down == False:
+                index+=1
+            if going_down==True:
+                index-=1
+            if index==numRows:
+                going_down=True
+            if index==1:
+                going_down=False
+            print(track_matrix[i],going_down, index,numRows)
+        print(track_matrix)
+        print(hashmap)
+        result = ''.join(hashmap[i] for i in hashmap)
+        return result
+        
