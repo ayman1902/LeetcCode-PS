26eb821588a5b4ad8d75dd531b509f3e0c948ff3
ayman1902 <49163010+ayman1902@users.noreply.github.com>
2024-09-23 17:03:44 +0100

    Time: 6333 ms (5.02%) | Memory: 28.9 MB (22.10%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -4,16 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if len(nums)==0:
+        n=len(nums)
+        if n==0:
             return 0
         nums_set=set(nums)
         lenght=0
         lenght_arr=[]
         j=0
-        for i in range(len(nums)):
-            if nums[i]-1 in nums_set:
-                continue
-            else:
+        for i in range(n):
+            if nums[i]-1 not in nums_set:
                 while nums[i]+j in nums_set:
                     j+=1
                     lenght+=1
