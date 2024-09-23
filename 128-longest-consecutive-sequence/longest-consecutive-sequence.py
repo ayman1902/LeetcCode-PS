2e1a52983bf79d482c6b939aeb2eeb2d05cc2d36
ayman1902 <49163010+ayman1902@users.noreply.github.com>
2024-09-23 17:05:09 +0100

    Time: 6469 ms (5.02%) | Memory: 28.9 MB (22.10%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -8,15 +8,14 @@ class Solution(object):
         if n==0:
             return 0
         nums_set=set(nums)
-        lenght=0
-        lenght_arr=[]
+        longest=0
         j=0
         for i in range(n):
             if nums[i]-1 not in nums_set:
+                lenght=0
                 while nums[i]+j in nums_set:
                     j+=1
                     lenght+=1
-                lenght_arr.append(lenght)
-                lenght=0
+                longest=max(longest,lenght)
                 j=0
-        return max(lenght_arr)
+        return longest
