15253d41db67923731c8127477962d741b03879f
ayman1902 <49163010+ayman1902@users.noreply.github.com>
2024-09-26 08:42:09 +0100

    Time: 2 ms (84.11%) | Memory: 45.7 MB (67.29%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int left =0;
+        for(int right=0;right<nums.length;right++){
+            if(nums[right]!=0){
+                int temp=0;
+                temp=nums[left];
+                nums[left]=nums[right];
+                nums[right]=temp;
+                left++;
+            }
+        }
+        
+    }
+}
