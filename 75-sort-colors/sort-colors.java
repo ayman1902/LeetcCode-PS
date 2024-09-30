6aaf997c249753f9abbf0554b63e9207e84b4522
ayman1902 <49163010+ayman1902@users.noreply.github.com>
2024-09-30 09:33:15 +0100

    Time: 2 ms (15.90%) | Memory: 41.5 MB (96.41%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -7,13 +7,10 @@ class Solution {
         }
         final int[] start={0}; // foreach need a final variable so we use array as a wrapper
         map.forEach((key,value)->{
-            System.out.println(key+";"+value);
             for(int a=start[0];a<start[0]+value;a++){
                 nums[a]=key;
             }
             start[0]+=value;
-        });
-        System.out.print(Arrays.toString(nums));
-        
+        });        
     }
 }
