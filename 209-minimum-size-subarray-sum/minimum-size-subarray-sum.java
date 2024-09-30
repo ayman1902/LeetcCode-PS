9f34ee9694c6a5c7e020b2d6ce2e16e5c0617036
ayman1902 <49163010+ayman1902@users.noreply.github.com>
2024-09-30 11:28:04 +0100

    Time: 344 ms (5.33%) | Memory: 55.3 MB (92.21%) - LeetSync

---
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int start=0;
+        int end=0;
+        int count=nums[0];
+        double min_lenght=Double.POSITIVE_INFINITY;
+        while (end<nums.length){
+            if(count<target){
+                end++;
+                if(end<nums.length){
+                    count+=nums[end];
+                }
+            }
+            else{
+                min_lenght=Math.min(min_lenght,end-start+1);
+                count-=nums[start];
+                start++;
+            }
+            System.out.println(end-start);
+        }
+        if(Double.isInfinite(min_lenght)==true){
+            return 0;
+        }
+        return (int) Math.round(min_lenght);
+    }
+}
