bed4bbde10480e4ca0955ac6d1a7a40da38fa8b4
ayman1902 <49163010+ayman1902@users.noreply.github.com>
2024-10-02 18:28:36 +0100

    Time: 10 ms (25.85%) | Memory: 46.6 MB (42.41%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,9 +1,7 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         ArrayList<int[]> merged =new ArrayList<>();
-        System.out.println(Arrays.deepToString(intervals));
         Arrays.sort(intervals,Comparator.comparingInt(a->a[0]));
-        System.out.println(Arrays.deepToString(intervals));
         for(int[] interval : intervals){
             if(merged.isEmpty() || merged.get(merged.size()-1)[1]<interval[0]){
                 merged.add(interval);
@@ -11,7 +9,6 @@ class Solution {
                 int[] lastInterval = merged.get(merged.size()-1);
                 merged.set(merged.size()-1,new int[]{lastInterval[0],Math.max(lastInterval[1],interval[1])});
             }
-             System.out.println(Arrays.toString(interval));
         }
 
         return merged.toArray(new int[merged.size()][2]);
