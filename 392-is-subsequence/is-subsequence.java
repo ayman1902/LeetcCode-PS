edf5ef270ec7df54731ecf3700796381faf22862
ayman1902 <49163010+ayman1902@users.noreply.github.com>
2024-10-02 14:37:41 +0100

    Time: 1 ms (93.27%) | Memory: 41.6 MB (26.41%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -3,7 +3,6 @@ class Solution {
         int j=0;
         int s_length=s.length();
         for(int i=0;i<t.length();i++){
-            System.out.println(t.charAt(i)+" "+j);
             if(j<s_length  &&  t.charAt(i)==s.charAt(j)){
                 j++;
             }
