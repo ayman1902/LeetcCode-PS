4ead1408dfa42b98e42580b8bea5d3e772938d3b
ayman1902 <49163010+ayman1902@users.noreply.github.com>
2024-10-02 21:07:55 +0100

    Time: 2 ms (31.36%) | Memory: 41.9 MB (42.32%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder merged=new StringBuilder();
+        String bigWord=new String();
+        int length_1=word1.length();
+        int length_2=word2.length();
+        int min_length=Math.min(length_1,length_2);
+        if(length_1>length_2){
+            bigWord=word1;
+        }else{
+            bigWord=word2;
+        }
+        for(int i=0;i<min_length;i++){
+            merged.append(word1.charAt(i)).append(word2.charAt(i));
+        }
+        for(int j=min_length;j<bigWord.length();j++){
+            merged.append(bigWord.charAt(j));
+        }
+        System.out.println(merged);
+        return merged.toString();
+        
+    }
+}
