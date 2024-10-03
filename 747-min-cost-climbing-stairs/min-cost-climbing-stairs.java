cf22d31b8f93fc5c7505ce7c6ef6e53d7ad2a541
ayman1902 <49163010+ayman1902@users.noreply.github.com>
2024-10-03 15:06:55 +0100

    Time: 0 ms (100.00%) | Memory: 42.7 MB (88.82%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -1,5 +1,6 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
+        /*
         ArrayList<Integer> dp =new ArrayList<>();
         dp.add(0);
         dp.add(0);
@@ -7,6 +8,15 @@ class Solution {
         for(int i=2;i<cost.length+1;i++){
             dp.add(Math.min(dp.get(i-1)+cost[i-1],dp.get(i-2)+cost[i-2]));
         }
-        return dp.get(dp.size()-1);
+        return dp.get(dp.size()-1);*/
+        int prev=0;
+        int current=0;
+        int temp=0;
+        for(int i=2;i<cost.length+1;i++){
+            temp=prev;
+            prev=current;
+            current=Math.min(temp+cost[i-2],current+cost[i-1]);
+        }
+        return current;
     }
 }
