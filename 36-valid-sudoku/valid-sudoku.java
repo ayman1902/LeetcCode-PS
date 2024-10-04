571dc1fae8fd806db676d78b0728f42ce4f5c0aa
ayman1902 <49163010+ayman1902@users.noreply.github.com>
2024-10-04 19:20:13 +0100

    Time: 13 ms (25.32%) | Memory: 44.3 MB (69.95%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.java
@@ -0,0 +1,55 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        //check rows
+        for(int i=0;i<9;i++){
+            HashSet<Integer> set = new HashSet<>();
+            for(int j=0;j<9;j++){
+                char item= board[i][j];
+                if(set.contains((int) item)){
+                    return false;
+                }
+                if(item!='.'){
+                    set.add((int) item);
+                }
+            }
+        }
+        //check columns
+         for(int i=0;i<9;i++){
+            HashSet<Integer> set = new HashSet<>();
+            for(int j=0;j<9;j++){
+                char item= board[j][i];
+                if(set.contains((int) item)){
+                    return false;
+                }
+                if(item!='.'){
+                    set.add((int) item);
+                }
+            }
+        }
+        //check boxes
+        int[][] starts ={{0,0},{0,3},{0,6},
+                         {3,0},{3,3},{3,6},
+                         {6,0},{6,3},{6,6}};
+
+        for(int[] box : starts){
+            int a=box[0];
+            int b=box[1];
+            HashSet<Integer> set = new HashSet<>();
+            for(int i=a;i<a+3;i++){
+                for(int j=b;j<b+3;j++){
+                    char item= board[i][j];
+                    if(set.contains((int) item)){
+                        return false;
+                    }
+                    if(item!='.'){
+                        set.add((int) item);
+                    }
+                }
+        }
+
+            System.out.println(Arrays.toString(box));
+        }
+        System.out.println(Arrays.deepToString(starts));
+        return true;
+    }
+}
