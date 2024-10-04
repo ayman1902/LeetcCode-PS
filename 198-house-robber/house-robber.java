744bb89dd7d668f950e153b0ec89300a3dcad02d
ayman1902 <49163010+ayman1902@users.noreply.github.com>
2024-10-04 18:40:37 +0100

    Time: 0 ms (100.00%) | Memory: 41.1 MB (36.02%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int rob(int[] nums) {
+        int res=0;
+        int nums_length=nums.length;
+        ArrayList<Integer> dp = new ArrayList<>();
+        if(nums_length==1){
+            return nums[0];
+        }
+        if(nums_length==2){
+            return Math.max(nums[0],nums[1]);
+        }
+        dp.add(nums[0]);
+        dp.add(Math.max(nums[0],nums[1]));
+        for(int i=2;i<nums_length;i++){
+            dp.add(Math.max(dp.get(i-1),nums[i]+dp.get(i-2)));
+        }
+        return dp.get(dp.size()-1);
+        
+    }
+}
