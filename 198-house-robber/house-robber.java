20654462bd652c5a42219cc47aef5a57461e6163
ayman1902 <49163010+ayman1902@users.noreply.github.com>
2024-10-04 18:41:58 +0100

    Time: 0 ms (100.00%) | Memory: 41.1 MB (25.75%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -9,6 +9,7 @@ class Solution {
         if(nums_length==2){
             return Math.max(nums[0],nums[1]);
         }
+        //we can use prev,curr to reduce spacial complexity
         dp.add(nums[0]);
         dp.add(Math.max(nums[0],nums[1]));
         for(int i=2;i<nums_length;i++){
