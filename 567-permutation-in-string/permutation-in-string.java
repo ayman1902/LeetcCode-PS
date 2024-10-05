ddff66ce66bca8ddab0f1d0b5043aa65e1b7d944
ayman1902 <49163010+ayman1902@users.noreply.github.com>
2024-10-05 19:47:15 +0100

    Time: 8 ms (60.46%) | Memory: 42.8 MB (47.69%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int[] s1_count = new int[26];
+        int[] s2_count = new int[26];
+        int s1_length = s1.length();
+        int s2_length = s2.length();
+        if(s1_length>s2_length){
+            return false;
+        }
+        for(int i=0;i<s1_length;i++){
+            s1_count[s1.charAt(i) - 'a']++;
+            s2_count[s2.charAt(i) - 'a']++;
+        }
+        if(Arrays.equals(s1_count,s2_count)){
+            return true;
+        }
+        for(int i=s1_length;i<s2_length;i++){
+            s2_count[s2.charAt(i) - 'a']++;
+            s2_count[s2.charAt(i-s1_length) - 'a']--;
+            if(Arrays.equals(s1_count,s2_count)){
+                 return true;
+            }
+        }
+        System.out.println(Arrays.toString(s1_count));
+        System.out.println(Arrays.toString(s2_count));
+        return false;
+        
+    }
+}
