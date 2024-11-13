f5120cfccf745e053e83cfde2e6577e62f849322
ayman1902 <49163010+ayman1902@users.noreply.github.com>
2024-11-13 20:22:58 +0100

    Time: 7182 ms (5.01%) | Memory: 20.4 MB (5.17%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -4,29 +4,20 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        nums.sort()
-        n=len(nums)
+        hashMap={}
+        s=set()
+        for i in range(len(nums)):
+            hashMap[nums[i]]=i
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                disered=-nums[i]-nums[j]
+                if(disered in hashMap and hashMap[disered]!=i and hashMap[disered]!=j):
+                    s.add(tuple(sorted([disered,nums[i],nums[j]])))
+        print(hashMap,s)
         res=[]
-        for k in range(n):
-            if k > 0 and nums[k] == nums[k - 1]:
-                continue
-            i=k+1
-            j=n-1
-            while i<j:
-                combin=nums[k]+nums[i]+nums[j]
-                if combin==0:
-                    res.append([nums[k],nums[i],nums[j]])
-                    while i < j and nums[i] == nums[i + 1]:
-                        i += 1
-                    while i < j and nums[j] == nums[j - 1]:
-                        j -= 1
-                    i += 1
-                    j -= 1
-                elif combin>0:
-                    j-=1
-                else:
-                    i+=1
+        for e in s:
+            res.append(list(e))
+            print(list(e))
         return res
-
 
         
